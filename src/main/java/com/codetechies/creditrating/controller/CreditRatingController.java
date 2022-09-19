e0eaa49f9814fe63b1089faac77759f8aff1f267
Amit <the.amisoft@gmail.com>
2022-09-19 18:35:31 +0530

    Test

--- src/main/java/com/codetechies/creditrating/controller/CreditRatingController.java
+++ src/main/java/com/codetechies/creditrating/controller/CreditRatingController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Random;
-
+//Test
 @RestController
 @Slf4j
 public class CreditRatingController {
